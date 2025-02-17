9fc1a66b780e6d582e001b8bedac38f6a8be4fa4
Jhockinn <Tobiln@hotmail.dk>
2025-02-17 14:52:12 +0100

    Initial commit

---
+++ .idea/js/guessnumber.js
@@ -0,0 +1,14 @@
+console.log("jeg er i guessnumber")
+
+const lblMessage = document.querySelector(".message");
+console.log(lblMessage);
+console.log(lblMessage.textContent)
+
+const lblNumber = document.querySelector(".number");
+console.log(lblNumber);
+
+const lblScore = document.querySelector(".score");
+console.log(lblScore);
+
+let inpGuess = document.querySelector(".guess");
+console.log(inpGuess);
