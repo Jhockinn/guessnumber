d2bc3664c066743128d46a2db4d30eb8c5b00282
Jhockinn <Tobiln@hotmail.dk>
2025-02-19 23:50:45 +0100

    updated

--- .idea/js/guessnumber.js
+++ .idea/js/guessnumber.js
@@ -3,12 +3,40 @@ console.log("jeg er i guessnumber")
 const lblMessage = document.querySelector(".message");
 console.log(lblMessage);
 console.log(lblMessage.textContent)
-
 const lblNumber = document.querySelector(".number");
 console.log(lblNumber);
-
 const lblScore = document.querySelector(".score");
 console.log(lblScore);
-
-let inpGuess = document.querySelector(".guess");
+const inpGuess = document.querySelector(".guess");
 console.log(inpGuess);
+const lblHighscore = document.querySelector(".highscore")
+const btnCheck = document.querySelector(".check")
+const btnAgain = document.querySelector(".again")
+
+
+let secretNumber = Math.floor(Math.random() * 20 + 1)
+let score = 20
+let highscore = 0
+
+console.log("Secret Number:", secretNumber)
+
+function testNumber(){
+    console.log("testNumber")
+    guess = inpGuess.value
+    console.log(inpGuess.value)
+
+    if(!guess){
+        lblMessage.textContent = "Wrong Number"
+    } else if (guess === secretNumber){
+        lblMessage.textContent = '?? You have Guessed the Number'
+        youWin()
+    } else if (guess < secretNumber){
+        lblMessage.textContent = 'Too Low'
+    } else if (guess > secretNumber){
+        lblMessage.textContent = 'Too High'
+    }
+}
+function youWin(){
+    console.log("You Win!")
+    lblNumber.textContent = secretNumber
+}
